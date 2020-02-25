92fe345ea6ea567e45a70769d40c04aa81365e19
Martin Makarsky <martinmakarsky@gmail.com>
2020-02-25 14:31:28 +0100

    Get site info for category template page from Kontent.

--- src/templates/tag-template.jsx
+++ src/templates/tag-template.jsx
@@ -4,18 +4,22 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Sidebar from '../components/Sidebar'
 import TagTemplateDetails from '../components/TagTemplateDetails'
+import { getSiteInfo } from '../utils/kontentItemNodeUtils'
 
 class TagTemplate extends React.Component {
   render() {
-    const { title } = this.props.data.site.siteMetadata
-    const { tag } = this.props.pageContext
+    let pageTemplateData = this.props;
+    pageTemplateData.data.site = getSiteInfo(pageTemplateData.data.kontentItemMenu, pageTemplateData.data.kontentItemAuthor, pageTemplateData.data.kontentItemSiteMetadata);
+
+    const { title } = pageTemplateData.data.site.siteMetadata
+    const { tag } = pageTemplateData.pageContext
 
     return (
       <Layout>
         <div>
           <Helmet title={`All Posts tagged as "${tag}" - ${title}`} />
-          <Sidebar {...this.props} />
-          <TagTemplateDetails {...this.props} />
+          <Sidebar {...pageTemplateData} />
+          <TagTemplateDetails {...pageTemplateData} />
         </div>
       </Layout>
     )
@@ -26,23 +30,60 @@ export default TagTemplate
 
 export const pageQuery = graphql`
   query TagPage($tag: String) {
-    site {
-      siteMetadata {
-        title
-        subtitle
-        copyright
-        menu {
-          label
-          path
+    kontentItemSiteMetadata(system: {codename: {eq: "site_metadata"}}) {
+      elements {
+        copyright {
+          value
         }
-        author {
-          name
-          email
-          telegram
-          twitter
-          github
-          rss
-          vk
+        subtitle {
+          value
+        }
+        title {
+          value
+        }
+      }
+    }
+    kontentItemMenu(system: {codename: {eq: "navigation_menu"}}) {
+      elements {
+        menu_items {
+          linked_items {
+            ... on KontentItemMenuItem {
+              id
+              elements {
+                label {
+                  value
+                }
+                path {
+                  value
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    kontentItemAuthor(system: {codename: {eq: "author"}}) {
+      elements {
+        email {
+          value
+        }
+        github {
+          value
+        }
+        name {
+          value
+        }
+        rss {
+          value
+        }
+        telegram {
+          value
+        }
+        twitter {
+          value
+        }
+        vk {
+          value
         }
       }
     }
