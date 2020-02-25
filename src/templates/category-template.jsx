32c1cfb893572aa82eef8d48776f0b36713a187d
Martin Makarsky <martinmakarsky@gmail.com>
2020-02-25 14:28:47 +0100

    Get site info for category template page from Kontent.

--- src/templates/category-template.jsx
+++ src/templates/category-template.jsx
@@ -4,18 +4,22 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Sidebar from '../components/Sidebar'
 import CategoryTemplateDetails from '../components/CategoryTemplateDetails'
+import { getSiteInfo } from '../utils/kontentItemNodeUtils'
 
 class CategoryTemplate extends React.Component {
   render() {
-    const { title } = this.props.data.site.siteMetadata
-    const { category } = this.props.pageContext
+    let categoryTemplateData = this.props;
+    categoryTemplateData.data.site = getSiteInfo(categoryTemplateData.data.kontentItemMenu, categoryTemplateData.data.kontentItemAuthor, categoryTemplateData.data.kontentItemSiteMetadata);
+
+    const { title } = categoryTemplateData.data.site.siteMetadata
+    const { category } = categoryTemplateData.pageContext
 
     return (
       <Layout>
         <div>
           <Helmet title={`${category} - ${title}`} />
-          <Sidebar {...this.props} />
-          <CategoryTemplateDetails {...this.props} />
+          <Sidebar {...categoryTemplateData} />
+          <CategoryTemplateDetails {...categoryTemplateData} />
         </div>
       </Layout>
     )
@@ -26,23 +30,60 @@ export default CategoryTemplate
 
 export const pageQuery = graphql`
   query CategoryPage($category: String) {
-    site {
-      siteMetadata {
-        title
-        subtitle
-        copyright
-        menu {
-          label
-          path
+    kontentItemSiteMetadata(system: {codename: {eq: "site_metadata"}}) {
+      elements {
+        copyright {
+          value
         }
-        author {
-          name
-          email
-          telegram
-          twitter
-          github
-          rss
-          vk
+        subtitle {
+          value
+        }
+        title {
+          value
+        }
+      }
+    }
+    kontentItemMenu(system: {codename: {eq: "navigation_menu"}}) {
+      elements {
+        menu_items {
+          linked_items {
+            ... on KontentItemMenuItem {
+              id
+              elements {
+                label {
+                  value
+                }
+                path {
+                  value
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    kontentItemAuthor(system: {codename: {eq: "author"}}) {
+      elements {
+        email {
+          value
+        }
+        github {
+          value
+        }
+        name {
+          value
+        }
+        rss {
+          value
+        }
+        telegram {
+          value
+        }
+        twitter {
+          value
+        }
+        vk {
+          value
         }
       }
     }
