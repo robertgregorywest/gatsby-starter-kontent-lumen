044cefba3b4ef60ea9371ec4596fbd2b10a83c6c
Martin Makarsky <martinmakarsky@gmail.com>
2020-03-06 16:59:13 +0100

    Remove gatsby-source-filesystem.

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,13 +37,6 @@ module.exports = {
     },
   },
   plugins: [
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
     // {
     //   resolve: 'gatsby-plugin-feed',
     //   options: {
