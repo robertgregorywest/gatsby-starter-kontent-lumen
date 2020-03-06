2e104baf980f7d5570d27c9f274b3448a4a1c087
Martin Makarsky <martinmakarsky@gmail.com>
2020-03-06 17:05:32 +0100

    Revert "Remove gatsby-source-filesystem."
    This reverts commit 044cefba3b4ef60ea9371ec4596fbd2b10a83c6c.

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,13 @@ module.exports = {
     },
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
     // {
     //   resolve: 'gatsby-plugin-feed',
     //   options: {
