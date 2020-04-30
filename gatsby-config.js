c7caee69a6b825419b6c6ca95788745791550693
ondrejch <ondrejch@kentico.com>
2020-04-30 13:32:21 +0200

    fix double negation

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,8 @@ module.exports = {
         projectId: process.env.KONTENT_PROJECT_ID, // Fill in your Project ID
         // if false used authorization key for secured API
         usePreviewUrl: !!process.env.KONTENT_PREVIEW_ENABLED,
-        authorizationKey: process.env.KONTENT_PREVIEW_ENABLED
+        // eslint-disable-next-line no-extra-boolean-cast
+        authorizationKey: !!process.env.KONTENT_PREVIEW_ENABLED
           ? process.env.KONTENT_PREVIEW_KEY
           : undefined,
         languageCodenames: process.env.KONTENT_LANGUAGE_CODENAMES.split(',').map(lang => lang.trim()),
