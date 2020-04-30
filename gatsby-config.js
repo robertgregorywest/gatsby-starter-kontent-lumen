e063288a0f37cba467da2d47fa43df9ec289dec6
ondrejch <ondrejch@kentico.com>
2020-04-30 13:37:04 +0200

    'false' is truthy :/

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
         projectId: process.env.KONTENT_PROJECT_ID, // Fill in your Project ID
         // if false used authorization key for secured API
         usePreviewUrl: !!process.env.KONTENT_PREVIEW_ENABLED,
-        authorizationKey: process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true'
+        authorizationKey: process.env.KONTENT_PREVIEW_ENABLED && process.env.KONTENT_PREVIEW_ENABLED.toLowerCase() === 'true'
           ? process.env.KONTENT_PREVIEW_KEY
           : undefined,
         languageCodenames: process.env.KONTENT_LANGUAGE_CODENAMES.split(',').map(lang => lang.trim()),
